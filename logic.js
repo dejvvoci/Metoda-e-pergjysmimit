1de113e48261437cef32869a3abbe8f3662a8ded
dejvvoci <dejvivoci2@gmail.com>
2024-05-10 10:42:03 +0200

    Logic code refactored

--- logic.js
+++ logic.js
@@ -17,48 +17,27 @@ document.getElementById("actionBtn").addEventListener("click", function(event){
 });
 
 function përgjysmo(a, b, funksioni, toleranca) {
-    // Përpunimi i shprehjes së dhënë nga përdoruesi në një funksion të vërtetë
     let str = funksioni.toString();
-    let regex = /(\w+)\*\*(\d+)|ln\((\w+)\)/; // Regex për të gjetur shprehjet e tipit "x**n" ose "ln(x)"
-    let matches = funksioni.match(regex); // Gjejmë shprehjen "x**n" ose "ln(x)"
-    // if (matches) {
-    //     if (matches[1]) { // Nëse është një shprehje e tipit "x**n"
-    //         let variabla = matches[1]; // variabla është "x" në rastin tonë
-    //         let eksponenti = parseInt(matches[2]); // eksponenti është "2" në rastin tonë
-
-    //         // Përshtatim i shprehjes për t'u përdorur si funksion në JavaScript
-    //         funksioni = function(x) {
-    //             return Math.pow(x, eksponenti) - 4;
-    //         };
-    //     } else 
-    // if (matches[3]) { // Nëse është një shprehje e tipit "ln(x)"
-    //         let variabla = matches[3]; // variabla është "x" në rastin tonë
-
-    //         // Përshtatim i shprehjes për t'u përdorur si funksion në JavaScript
-    //         funksioni = new Function('x', 'return ' + funksioni);
-    //     // }
-    // } else {
-        // Në rast se nuk gjendet shprehje e caktuar, përdorim shprehjen siç është
-        if(str.includes('ln(')){
-            str = str.replace('ln(', 'Math.log(');
-        }
-        if(str.includes('sqrt(')){
-            str = str.replace('sqrt(', 'Math.sqrt(');
-        }
-        if(str.includes('sin(')){
-            str = str.replace('sin(', 'Math.sin(');
-        }
-        if(str.includes('cos(')){
-            str = str.replace('cos(', 'Math.cos(');
-        }
-        if(str.includes('log10(')){
-            str = str.replace('log10(', 'Math.log10(');
-        }
-        if(str.includes('exp(')){
-            str = str.replace('exp(', 'Math.exp(');
-        }
-        funksioni = new Function('x', 'return ' + str);
-    // }
+    if(str.includes('ln(')){
+        str = str.replace('ln(', 'Math.log(');
+    }
+    if(str.includes('sqrt(')){
+        str = str.replace('sqrt(', 'Math.sqrt(');
+    }
+    if(str.includes('sin(')){
+        str = str.replace('sin(', 'Math.sin(');
+    }
+    if(str.includes('cos(')){
+        str = str.replace('cos(', 'Math.cos(');
+    }
+    if(str.includes('log10(')){
+        str = str.replace('log10(', 'Math.log10(');
+    }
+    if(str.includes('exp(')){
+        str = str.replace('exp(', 'Math.exp(');
+    }
+    funksioni = new Function('x', 'return ' + str);
+   
     console.log(`${a}, ${b}`);
     console.log(funksioni);
     
@@ -66,16 +45,18 @@ function përgjysmo(a, b, funksioni, toleranca) {
     let fb = funksioni(b);
     
     console.log(`${fa}, ${fb}`);
+
+    // JO i sigurt nqs kjo duhet
     // if (fa * fb >= 0) {
     //     console.log("Metoda e përgjysmimit nuk garanton konvergjencë në këtë interval.");
     //     return null;
     // }
 
-        let output = document.createElement('div');
-        var header = document.createElement("h2");
-        header.textContent = "Sequence of steps";
-        output.appendChild(header);
-        let count = 1;
+    let output = document.createElement('div');
+    var header = document.createElement("h2");
+    header.textContent = "Sequence of steps";
+    output.appendChild(header);
+    let count = 1;
 
     let c;
     while (Math.abs(b - a) > toleranca) {
